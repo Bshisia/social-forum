fefeacf5101d0d2e6a1aecefea5503048df60b63
Bshisia <shisiabrian7@gmail.com>
2025-03-10 19:10:17 +0300

    feat: fetch the login details from the json files instead of fetching the form values

--- authentication/signinhandler.go
+++ authentication/signinhandler.go
@@ -2,106 +2,98 @@ package handlers
 
 import (
 	"database/sql"
-	"html/template"
+	"encoding/json"
+	"forum/utils"
 	"log"
 	"net/http"
-
-	"forum/utils"
 )
 
-type SignInData struct {
-	Username      string
-	UsernameError string
-	PasswordError string
-	GeneralError  string
-}
-
-func SignInHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := r.Cookie("session_token")
-	if err == nil {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-
-	tmpl, err := template.ParseFiles("templates/signin.html")
-	if err != nil {
-		utils.RenderErrorPage(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
-		log.Printf("Error loading template: %v", err)
-		return
-	}
-
-	if r.Method == "GET" {
-		tmpl.Execute(w, nil)
-		return
-	}
-
-	if r.Method == "POST" {
-		data := SignInData{}
-
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-
-		if username == "" {
-			data.UsernameError = "Username is required"
-		}
-
-		if password == "" {
-			data.PasswordError = "password is required"
-		}
-
-		if data.UsernameError != "" || data.PasswordError != "" {
-			data.Username = username
-			tmpl.Execute(w, data)
-			return
-		}
-
-		var user utils.User
-		err := GlobalDB.QueryRow(`
-			SELECT id, password
-			FROM users
-			WHERE username = ?
-		`, username).Scan(&user.ID, &user.Password)
-		if err != nil {
-			data := struct {
-				GeneralError string
-				Username     string
-			}{
-				GeneralError: "Invalid username or password",
-				Username:     username,
-			}
-			tmpl, _ := template.ParseFiles("templates/signin.html")
-			tmpl.Execute(w, data)
-			if err != sql.ErrNoRows {
-				log.Printf("Error querying database: %v", err)
-			}
-			return
-		}
-
-		if !utils.CheckPasswordsHash(password, user.Password) {
-			data.GeneralError = "Invalid username or password"
-			data.Username = username
-			tmpl.Execute(w, data)
-			return
-		}
-
-		sessionToken, err := utils.CreateSession(GlobalDB, user.ID)
-		if err != nil {
-			utils.RenderErrorPage(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-			return
-		}
-
-		http.SetCookie(w, &http.Cookie{
-			Name:     "session_token",
-			Value:    sessionToken,
-			Path:     "/",
-			HttpOnly: false,
-			Secure:   false,
-			SameSite: http.SameSiteLaxMode,
-			MaxAge:   24 * 60 * 60,
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"message": "Invalid request method",
+			"success": false,
 		})
-
-		log.Printf("Set session cookie: %s", sessionToken)
-
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
+
+	var credentials struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"message": "Invalid request body",
+			"success": false,
+		})
+		return
+	}
+
+	var storedPassword string
+	var userId string
+	var nickname string
+	err = GlobalDB.QueryRow("SELECT id, password, nickname FROM users WHERE email = ?", credentials.Email).Scan(&userId, &storedPassword, &nickname)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"message": "Invalid email or password",
+				"success": false,
+			})
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"message": "Failed to query database",
+				"success": false,
+			})
+		}
+		return
+	}
+
+	isValidPassword := utils.CheckPasswordsHash(storedPassword, credentials.Password)
+	if !isValidPassword {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"message": "Invalid email or password",
+			"success": false,
+		})
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message":  "Login successful",
+		"success":  true,
+		"userId":   userId,
+		"nickname": nickname,
+	})
+
+	sessionToken, err := utils.CreateSession(GlobalDB, userId)
+	if err != nil {
+		utils.RenderErrorPage(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken,
+		Path:     "/",
+		HttpOnly: false,
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+		MaxAge:   24 * 60 * 60,
+	})
+
+	log.Printf("Set session cookie: %s", sessionToken)
+
 }
