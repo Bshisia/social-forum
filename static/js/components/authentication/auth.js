03fa1c7ddd5911cef7fee8e85400180c925e2991
bshisia <shisiabrian7@gmail.com>
2025-03-11 13:20:02 +0300

    feat: update the navigation bases on the authentication status

--- static/js/components/authentication/auth.js
+++ static/js/components/authentication/auth.js
@@ -21,3 +21,23 @@ function checkAuthState() {
         return false;
     }
 }
+
+// Update navigation based on authentication status
+function updateNavigation(authenticated) {
+    const nav = document.querySelector('nav ul');
+    
+    if (authenticated) {
+        nav.innerHTML = `
+            <li><a href="/" onclick="event.preventDefault(); loadPage('home')">Home</a></li>
+            <li><a href="/posts" onclick="event.preventDefault(); loadPage('posts')">Posts</a></li>
+            <li><a href="/createPost" onclick="event.preventDefault(); loadPage('createPost')">Create Post</a></li>
+            <li><a href="/messages" onclick="event.preventDefault(); loadPage('messages')">Messages</a></li>
+            <li><a href="#" onclick="event.preventDefault(); logout()">Logout</a></li>
+        `;
+    } else {
+        nav.innerHTML = `
+            <li><a href="/register" onclick="event.preventDefault(); loadPage('register')">Register</a></li>
+            <li><a href="/login" onclick="event.preventDefault(); loadPage('login')">Login</a></li>
+        `;
+    }
+}
